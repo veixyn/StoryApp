f0117bbda162ba6e0d95e18376654daf87d24da8
Tangana Vito Fortunata <tanganavito321@gmail.com>
2024-05-26 20:51:29 +0700

    Fixed hardcoded string on PasswordEditText

--- app/src/main/java/com/dicoding/storyapp/view/customUI/PasswordEditText.kt
+++ app/src/main/java/com/dicoding/storyapp/view/customUI/PasswordEditText.kt
@@ -3,6 +3,7 @@ package com.dicoding.storyapp.view.customUI
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
+import com.dicoding.storyapp.R
 
 class PasswordEditText(context: Context, attrs: AttributeSet?) : AppCompatEditText(context, attrs) {
 
@@ -13,7 +14,7 @@ class PasswordEditText(context: Context, attrs: AttributeSet?) : AppCompatEditTe
         lengthAfter: Int,
     ) {
         if (text.toString().length < 8) {
-            setError("Password tidak boleh kurang dari 8 karakter", null)
+            setError(context.getString(R.string.password_error), null)
         } else {
             error = null
         }
